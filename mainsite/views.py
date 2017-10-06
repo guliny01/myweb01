e681828a59c80d74c6b6875a532dec3b75f36843
guliny01 <19483834@qq.com>
2017-10-05 20:28:08 -0700

    app mainsite commit

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists = list()
+    for count,body in enumerate(posts):
+        post_lists.append("No.{}:".format(str(count)) + str(body.title)+"<br>")
+        post_lists.append(str(body.slug)+"<br>")
+        post_lists.append("<larger>" + str(body.body.encode('utf-8'))\
+    +"</small><br><br>")
+    return HttpResponse(post_lists)
